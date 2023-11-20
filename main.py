0af540e77b7531fbfb30348fda47ed2a47df020e
hrasek <hosekmotor@seznam.cz>
2023-11-20 19:56:12 +0100

    Second version of API.

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
-
 
 class Item(BaseModel):
     name: str
+    description: Union[str, None] = None
     price: float
-    is_offer: Union[bool, None] = None
+    tax: Union[float, None] = None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+async def read_item(item_id: int):
+    return {"item_id": item_id}
