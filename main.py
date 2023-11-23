725f068ab09271985752b1abd1ee2d603d674a10
hrasek <hosekmotor@seznam.cz>
2023-11-23 14:59:24 +0100

    Improved the create item function so it actually creates a file.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import ast
 
 class Item(BaseModel):
     name: str
@@ -21,9 +21,21 @@ async def create_item(item: Item):
     if item.tax:
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-
+    file_name = 'first_api.txt'
+    try:
+        file_txt = open(file_name, 'r')
+        content_list = file_txt.readlines()
+        last_line = content_list[-1]
+        last_line_striped = last_line[0:-1] 
+        last_line_dict = ast.literal_eval(last_line_striped)
+        file_id = last_line_dict.get('file_id') + 1
+        file_txt.close()
+    except FileNotFoundError:
+        file_id = 0
+    file_txt = open(file_name, mode = 'a')
+    item_dict.update({'file_id': file_id})
+    file_txt.write(str(item_dict) + '\n')
+    file_txt.close()
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
