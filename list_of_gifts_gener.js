8551872989791522baa3716e8fbae8e93398c87f
hrasek <hosekmotor@seznam.cz>
2023-12-13 13:49:19 +0100

    Started building front-end for the api.

--- list_of_gifts_gener.js
+++ list_of_gifts_gener.js
@@ -1,6 +1,24 @@
-function Add_gift() {
-  document.getElementById("bezdietyOutput").innerHTML =
-    "Hovězí vývar s domácími játrovými knedlíčky, nudlemi a zeleninou <br> Svíčková na smetaně s karlovarským knedlíkem, šlehačkou a brusinkami";
+function Get_gift() {
+  const apiUrl = "http://127.0.0.1:8000/items/0";
+
+  // Using the fetch function to make a GET request
+  fetch(apiUrl)
+    .then((response) => {
+      // Check if the response status is OK (status code 200)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      // Parse the JSON content of the response
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the data from the local API
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle errors that may occur during the fetch
+      console.error("Fetch error:", error);
+    });
 }
 // function Bezlepku() {
 //   document.getElementById("bezlepkuOutput").innerHTML =
