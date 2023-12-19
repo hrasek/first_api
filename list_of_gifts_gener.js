6617e5d459880bfce40d3eef612f444ddd2c6359
hrasek <hosekmotor@seznam.cz>
2023-12-19 16:20:36 +0100

    Added add gift functionality.

--- list_of_gifts_gener.js
+++ list_of_gifts_gener.js
@@ -1,5 +1,5 @@
 function Get_gift(table) {
-  var userInput = document.getElementById("userInput").value;
+  var userInput = document.getElementById("InputID").value;
   const apiUrl = "http://127.0.0.1:8000/items/" + userInput.toString();
 
   fetch(apiUrl)
@@ -15,6 +15,39 @@ function Get_gift(table) {
     .catch((error) => {
       console.error("Fetch error:", error);
     });
+}
+function Add_gift() {
+  const apiUrl = "http://127.0.0.1:8000/items/";
+  var inputName = document.getElementById("InputName").value;
+  try {
+    var InputDescription = document.getElementById("InputDescription").value;
+  } catch {
+    var InputDescription = null;
+  }
+  var InputPrice = document.getElementById("InputPrice").value;
+  try {
+    var InputTax = document.getElementById("InputTax").value;
+    //var InputTax = 0.0;
+  } catch {
+    var InputTax = 0.0;
+  }
+  data = {
+    name: inputName,
+    description: InputDescription,
+    price: InputPrice,
+    tax: InputTax,
+  };
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Specify the content type if sending JSON data
+      // Add other headers if needed
+    },
+    body: JSON.stringify(data), // Convert data to JSON format
+  })
+    .then((response) => response.json()) // Parse the response as JSON
+    .then((data) => console.log("Success:", data))
+    .catch((error) => console.error("Error:", error));
 }
 
 function displayData(data, table) {
@@ -36,7 +69,14 @@ function generateTableHeader() {
   var table = document.createElement("table");
   var headerRow = table.insertRow();
   // var keys = Object.keys(data);
-  var headers = ["name", "description", "price", "tax", "item_id"];
+  var headers = [
+    "name",
+    "description",
+    "price",
+    "tax",
+    "price with tax",
+    "item_id",
+  ];
   for (let head in headers) {
     var headerCell = headerRow.insertCell();
     txt = headers[head];
